4502ba937172a3f8e05a565ef12faeff6dd644e6
Zach Quita <zachquita@gmail.com>
2024-03-07 12:46:45 +0800

    Add animations toggle keybind

--- .local/bin/gamemode.sh
+++ .local/bin/gamemode.sh
@@ -2,7 +2,7 @@
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ]; then
 	hyprctl --batch "\
-        keyword animations:enabled 0;"
+        keyword animations:enabled 0;" && notify-send "Animations disabled"
 	exit
 fi
-hyprctl reload
+hyprctl reload && notify-send "Animations enabled"
